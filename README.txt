e6feb3fee14074e807f28e56678f2c75e5b98409
madenis <madenis@mailoo.org>
2016-12-17 11:06:57 +0100

    Ajout menu dans hello.sh et mise a jour README

--- README.txt
+++ README.txt
@@ -1,2 +1,7 @@
 # Exercice - Git
-## Objectif: S'entrainer à utiliser GIT
+## Objectif: 
+- S'entrainer à utiliser GIT
+- Creer un menu d'accueil
+
+## Amelioration
+- L'appel au script bye.sh dans hello.sh n'est pas fiable
--- hello.sh
+++ hello.sh
@@ -6,3 +6,16 @@ echo "Hello ${USER}!"
 #Donne la date
 echo "Nous sommes le `date +%A" "%d" "%B" il est "%H":"%M`"
 
+#Menu
+echo -n "Que veux tu faire?
+1 - Choix1
+2 - Choix2
+3 - Choix3
+0 - Quit
+
+Réponse: "
+
+read answer
+if [ "$answer" = "0" ] ; then
+    bash bye.sh
+fi
